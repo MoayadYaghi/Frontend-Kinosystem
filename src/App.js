7a7e5767d7e947ce72e9f2a213dc9b412291a306
SiepmannC <56861841+SiepmannC@users.noreply.github.com>
2020-12-11 22:46:38 +0100

    Hinzufügen der SliderID

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 import React, { useState } from "react";
 import "./App.css";
-import MoviesSlider from "./Components/Slider/Slider";
-import Navbar from "./Components/Navbar/Navbar";
-import Navbar1 from "./Components/Footer/Navbar";
-import Footer from "./Components/Footer/Footer";
+import MoviesSlider from "./components/Slider/Slider";
+import Navbar from "./components/Navbar/Navbar";
+//import Navbar1 from "./components/Footer/Navbar";
+import Footer from "./components/Footer/Footer";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
 
@@ -11,6 +11,8 @@ function App() {
 
   const [query, setQuery] = useState("");
   const [images, setImages] = useState([]);
+  const [id, setid] = useState([]);
+
   const runQuery = () => {
     fetch(
       `https://pixabay.com/api/?key=17555646-e40df9968035314583cab5eca&q=${query}`
@@ -18,14 +20,23 @@ function App() {
     .then((response) => response.json())
     .then(({ hits }) => hits.map(({ webformatURL }) => webformatURL))
     .then(setImages);
+  
+    fetch(
+      `https://pixabay.com/api/?key=17555646-e40df9968035314583cab5eca&q=${query}`
+    )
+    .then((response) => response.json())
+    .then(({ hits }) => hits.map(({ id }) => id))
+    .then(setid);
   }
 
   return (
     <div className="App">
       <Navbar/>
-      <MoviesSlider images={images}/>
-      <button onClick={runQuery}>Test Button 1</button>
+      <MoviesSlider images={images} text = {id}/>
+      
+      <button onClick ={runQuery}>Test Button 1</button>
       <button onClick={(e) => setQuery(e.target.value)}>Test Button 2</button>
+      
       <Footer />
     </div>
   );
