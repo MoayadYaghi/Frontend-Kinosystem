aee1114ea1b1c52ea40358c6a88083c0851c81a8
Moayad_Yaghi <moayad.yaghi@sap.com>
2020-12-17 23:32:39 +0100

    commit

--- src/App.js
+++ src/App.js
@@ -8,7 +8,6 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
 
 function App() {
-
   const [query, setQuery] = useState("");
   const [images, setImages] = useState([]);
   const [id, setid] = useState([]);
@@ -17,23 +16,23 @@ function App() {
     fetch(
       `https://pixabay.com/api/?key=17555646-e40df9968035314583cab5eca&q=${query}`
     )
-    .then((response) => response.json())
-    .then(({ hits }) => hits.map(({ webformatURL }) => webformatURL))
-    .then(setImages);
-  
+      .then((response) => response.json())
+      .then(({ hits }) => hits.map(({ webformatURL }) => webformatURL))
+      .then(setImages);
+
     fetch(
       `https://pixabay.com/api/?key=17555646-e40df9968035314583cab5eca&q=${query}`
     )
-    .then((response) => response.json())
-    .then(({ hits }) => hits.map(({ id }) => id))
-    .then(setid);
-  }
+      .then((response) => response.json())
+      .then(({ hits }) => hits.map(({ id }) => id))
+      .then(setid);
+  };
 
   return (
     <div className="App">
-      <Navbar/>
-      <MoviesSlider images={images} text = {id}/>
-      <button onClick ={runQuery}>Call API</button>
+      <Navbar />
+      <MoviesSlider images={images} text={id} />
+      <button onClick={runQuery}>Call API</button>
       <Footer />
     </div>
   );
