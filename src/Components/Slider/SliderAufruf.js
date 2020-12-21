f4991470d5a4bef175b38d75ee0621581cb2a038
Moayad_Yaghi <moayad.yaghi@sap.com>
2020-12-21 16:09:41 +0100

    fomat document

--- src/Components/Slider/SliderAufruf.js
+++ src/Components/Slider/SliderAufruf.js
@@ -1,52 +1,33 @@
-import React from 'react'
-import MoviesSlider from './Slider'
-import SliderPicture from '../../API_Pulls/SliderPicture'
+import React from "react";
+import MoviesSlider from "./Slider";
+import SliderPicture from "../../API_Pulls/SliderPicture";
 
+class SliderAufruf extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      Bilder: [],
+    };
+  }
 
-class SliderAufruf extends React.Component{
-    
+  componentDidMount() {
+    SliderPicture.getSliderImage().then((response) => {
+      console.log(response);
+      let BilderAPI = response.data.hits;
+      console.log(BilderAPI);
 
-    constructor(props){
-        super(props)
-        this.state = {
-            Bilder:[]
-        }
-    }
-    
+      this.setState({ Bilder: BilderAPI });
+    });
+  }
 
-    componentDidMount(){
-       
-       
-        SliderPicture.getSliderImage().then((response) => {
-            console.log(response);
-            let BilderAPI = response.data.hits;
-            console.log(BilderAPI)
-            
-            this.setState({Bilder: BilderAPI})
-            
-        })
-    }
-        
-
-    
-render(){
-
-    
-    
-
-return ( 
-    <div>
-                    
-                    {
-                    this.state.Bilder.map( bi =>
-                        
-                        
-                     <MoviesSlider key= {bi.id} images={bi.webformatURL} text = {bi.id}/> 
-                    
-                    )}
-                </div>
-
-)
-        }
+  render() {
+    return (
+      <div>
+        {this.state.Bilder.map((bi) => (
+          <MoviesSlider key={bi.id} images={bi.webformatURL} text={bi.id} />
+        ))}
+      </div>
+    );
+  }
 }
 export default SliderAufruf;
--- src/Components/Slider/SliderInfoComponent.js
+++ src/Components/Slider/SliderInfoComponent.js
@@ -1,84 +1,57 @@
-import React from 'react'
-import MoviesSlider from './Slider'
-import SliderAPI from '../../API_Pulls/SliderAPI'
-import axios from 'axios'
+import React from "react";
+import MoviesSlider from "./Slider";
+import SliderAPI from "../../API_Pulls/SliderAPI";
+import axios from "axios";
 
-class SliderInfoComponent extends React.Component{
-    
+class SliderInfoComponent extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      informationen: [],
+    };
+  }
 
-    constructor(props){
-        super(props)
-        this.state = {
-            informationen:[]
-        }
-    }
-    
+  componentDidMount() {
+    SliderAPI.getSliderApi().then((response) => {
+      console.log(response);
+      let movies = response.data.results;
+      console.log(movies.image);
 
-    componentDidMount(){
-       
-       
-        SliderAPI.getSliderApi().then((response) => {
-            console.log(response);
-            let movies = response.data.results;
-            console.log(movies.image)
-            
-            this.setState({informationen: movies})
-            console.log(this.state.informationen)
-        })
-    }
-        
+      this.setState({ informationen: movies });
+      console.log(this.state.informationen);
+    });
+  }
 
-    
-render(){
+  render() {
+    return (
+      <div>
+        <h1 className="text-center">Namen Liste</h1>
+        <table className="table table-striped" style={{ marginLeft: 18 + "%" }}>
+          <thead>
+            <tr>
+              <td>Film ID</td>
+              <td>Film name</td>
+            </tr>
+          </thead>
+          <tbody>
+            {this.state.informationen.map((infos) => (
+              <tr key={infos.id}>
+                <td> {infos.id}</td>
+                <td> {infos.title}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+        {this.state.informationen.map((infos) => (
+          <div>
+            <div>{infos.id}</div>
 
-    
-    
-
-return ( 
-    
-    
-    
-    
-    <div>
-                    <h1 className = "text-center"> Namen Liste </h1>
-                    <table className = "table table-striped" style={{marginLeft: 18 + '%'}}>
-                        <thead>
-                            <tr>
-
-                                <td>Film ID</td>
-                                <td>Film name</td>
-                                
-                            </tr>
-                        </thead>
-                        <tbody>
-                            {
-                                this.state.informationen.map(
-                                    infos => 
-                                    <tr key = {infos.id}>
-                                        <td> {infos.id}</td>
-                                        <td> {infos.title}</td>
-                                                               
-
-                                    </tr>
-                                )
-                            }
-
-                        </tbody>
-                    </table>
-                    {
-                    this.state.informationen.map( infos =>
-                        <div>
-                        <div>
-                            {infos.id}
-                            </div>
-
-                            <img src={infos.image} style={{height: '200px'}}></img>
-                    {/* <MoviesSlider key= {infos.id} images={infos.image} text = {infos.id}/> */}
-                    </div>
-                    )}
-                </div>
-
-)
-        }
+            <img src={infos.image} style={{ height: "200px" }}></img>
+            {/* <MoviesSlider key= {infos.id} images={infos.image} text = {infos.id}/> */}
+          </div>
+        ))}
+      </div>
+    );
+  }
 }
 export default SliderInfoComponent;
