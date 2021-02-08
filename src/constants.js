f2fcc962ab333b2c0b1b539febc03ccf7827fafa
I517970 <niklas.schaefer@sap.com>
2021-02-08 11:45:27 +0100

    .

--- src/constants.js
+++ src/constants.js
@@ -1,4 +1,4 @@
-//const LocalURL = "http://13.73.145.87:8081/";
-const LocalURL = "http://localhost:8081/";
+const LocalURL = "http://13.73.145.87:8081/";
+//const LocalURL = "http://localhost:8081/";
 
 export default LocalURL;
